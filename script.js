c57413252b8a5b8a5dd5dbaebc12604f55217112
Muhammad Abbas <MuhammadAbbas1011@outlook.com>
2024-07-21 13:28:54 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+    console.log("this is how you create \"double  quotation\" inside of the brackets. You just use two \\, each one before the quotation mark that you're going to be using")
+
+    console.log('However, another way of doing this is to just start the mainline with single quotes and then paste a file path for example: "https://www.youtube.com/watch?v=PkZNo7MFNFg"')

05c86956d7fd7a7fbda0abf65f60957352c71434
Abbas <MuhammadAbbas1011@outlook.com>
2026-09-25 01:47:43 +0800

    added license and mem-game.js

--- images/README.md
+++ images/README.md
@@ -3,11 +3,8 @@
 <br>
 <p style="font-size: 5px"><em>Disclaimer: I take no credit for any of the artwork used in this project, I relied on human made artwork and have done my best to credit all original authors.</em></p>
 
-rps.gif = Sourced from --x--
-Original Author: 
+rps.gif = [Sourced from](https://x.com/lennsan_/status/833314723567394816) \
+Original Author: [lennart butz](https://x.com/lennsan_)
 
-xyz.gif = Sourced from --x--
-Original Author: 
-
-mem-games folder = Sourced from https://github.com/kubowania/memory-game/tree/master/images
-Original Author: Ania Kubow
+mem-games folder = [Sourced from](https://github.com/kubowania/memory-game/tree/master/images) \
+Original Author: [Ania Kubow](https://github.com/kubowania)
--- js/mem-game.js
+++ js/mem-game.js
@@ -1,6 +1,75 @@
 const cardArray = [
     {
         name: 'fries',
-        img: 'images/rps.png'
+        img: 'images/mem-game/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/mem-game/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/mem-game/hotdog.png'
+    },
+    {
+        name: 'blank',
+        img: 'images/mem-game/blank.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/mem-game/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/mem-game/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/mem-game/pizza.png'
+    },
+    {
+        name: 'white',
+        img: 'images/mem-game/white.png'
     }
 ]
+
+cardArray.sort(() => 0.5 - Math.random())   // sorts the array randomly - refer to bottom of document for explanation 
+
+const gridDisplay = document.querySelector('#grid')  // the hashtag means its looking for an ID
+console.log(gridDisplay)
+
+function createBoard () {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('img')
+        
+    }
+}
+
+
+
+// explanation by C. Code for cardArray.sort(() => 0.5 - Math.random())
+/*
+What sort() does:
+sort() takes a comparison function that returns: 
+a negative number (which will cause the first item to come before the second)
+a positive number (which will cause the first item to come AFTER the second)
+
+What this code does:
+Math.random() generates a decimal between 0 and 1. So 0.5 - Math.random() gives you:
+A negative number roughly half the time (when Math.random() is > 0.5)
+A positive number roughly half the time (when Math.random() is < 0.5)
+
+It's a hacky shuffle — not a perfect algorithm (the randomness isn't evenly distributed), but good enough for casual shuffling like card games.
+
+For true random shuffling use the Fisher-Yates shuffle instead
+*/
+
+/*
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+*/
